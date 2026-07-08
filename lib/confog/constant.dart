4a064e89d0c7b69e48fd79596917942655ee35a1
babak518 <146018985+babak518@users.noreply.github.com>
2026-07-08 22:16:42 +0330

    Create constant.dart
    add config/constant.dart

---
+++ lib/confog/constant.dart
@@ -0,0 +1,7 @@
+class BDConfig {
+  static const int minSides = 3;
+  static const int maxSides = 100;
+  static const int minDivisions = 1;
+  static const int maxDivisions = 50;
+  static const double defaultRadius = 150.0;
+}
